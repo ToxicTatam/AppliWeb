0f98696a2afd2a0c4b2f3213218e498492a2d2c6
LE NULZEC Leandre <llc0251@palpatine.enseeiht.fr>
2025-05-03 19:38:56 +0200

    MatchCard, matches et matches/[matchId] 1st try

--- web-app/src/app/(pages)/matches/[matchId]/page.jsx
+++ web-app/src/app/(pages)/matches/[matchId]/page.jsx
@@ -1,9 +1,55 @@
+"use client"
+import { useState, useEffect } from 'react';
+import Sidebar from '@/component/layout/Sidebar';
+import Footer from '@/component/layout/Footer';
+import { useRouter } from 'next/navigation';
+import {useAuth} from "./hooks/useAuth";
+import Header from "@/component/layout/Header";
 import React from 'react';
-const Page = () => {
+import MatchCard from '../../../component/matches/MatchCard';
+
+const Page = (matchId) =>{
     return (
-        <footer>
-            <p>demo</p>
-        </footer>
+        <div className="min-h-screen flex flex-col">
+            {/* Header */}
+            <header className="bg-blue-600 text-white p-6">
+                <div className="container mx-auto flex justify-between items-center">
+                    <h1 className="text-2xl font-bold">
+                        <Link href="/public">Match</Link>
+                    </h1>
+                    <nav>
+                        <ul className="flex space-x-4">
+                            <li>
+                                <Link href="/login" className="hover:underline">
+                                    Connexion
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/register" className="hover:underline">
+                                    Inscription
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>            
+
+            {/* Gallery Section */}
+            <section className="bg-blue-50 py-12">
+                <div className="container mx-auto text-center">
+                   {id ? <MatchCard matchId={matchId}/> : <p>Chargement...</p> } 
+                </div>
+            </section>
+
+            {/* Footer */}
+            <footer className="bg-blue-600 text-white py-6">
+                <div className="container mx-auto text-center">
+                    <p className="text-sm">
+                        &copy; 2024 Gestion Sportive. Tous droits réservés.
+                    </p>
+                </div>
+            </footer>
+        </div>
     );
 };
 
--- web-app/src/app/component/matches/MatchCard.jsx
+++ web-app/src/app/component/matches/MatchCard.jsx
@@ -1,10 +1,95 @@
 import React from 'react';
-const MatchCard = () => {
-    return (
-        <footer>
-            <p>demo</p>
-        </footer>
+const PlayerStatsCard = ({matchId}) => {
+    const [match, setMatch] = setUsage(null);
+    const [loading, setLoading] = setUsgae(true);
+    
+    useEffect(() => {
+        if (!matchId) throw new Error("No matchId to fetch data");;
+
+        const fetchData = async () => {
+            try { 
+                setLoading(true);
+                const matchData = await fetch("http://localhost:8080/api/" + matchId);
+
+                if (!matchData) throw new Error("Failure to load match informations");
+
+                const matchObj = await matchData.json();
+
+                setMatch(matchObj);
+            } catch (err){
+                console.log(err);
+            } finally{
+                setLoading(false);
+            } 
+        };
+
+        fetchData();
+    },[playerId]);
+  
+
+    if (loading) return <p>Chargement...</p>;
+
+    return(
+<div class="imso_mh__tm-scr imso_mh__mh-bd">
+    <div>
+        <div class="imso_mh__stts-l imso-ani imso_mh__stts-l-cont" style="">
+            <div class="imso_mh__pst-m-stts-l">
+                <span class="imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc" aria-label="Score à la fin du temps réglementaire">{match.getMatchStatus().toString()}</span>
+                <div class="imso-hide-overflow">
+                    <span>
+                        <span>
+                            <span jscontroller="zhya9d" class="imso-loa imso-ln" role="link" style="color:#162E58" tabindex="0" jsdata="QRQcFf;_;ByyJhM" jsaction="rcuQ6b:npT2md;dnGRzb" data-ved="2ahUKEwiQiYzb3oeNAxXPZ0EAHYurCPIQr056BAgCEAk">
+                                {match.getCompetition().getName()} 
+                            </span>
+                        </span>
+                        <span class="imso_mh__bull"> · </span>
+                    </span>
+                    <span>
+                        {match.getMatchDate().toString()} 
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="imso_mh__tm-a-sts">
+            <div class="imso-ani imso_mh__tas" style="">
+                <div class="imso_mh__ts-nee">
+                    <div class="imso_mh__first-tn-ed imso_mh__tnal-cont imso-loa imso-ut imso-tnol" jscontroller="QhKwbc" data-df-team-mid="/m/0xbm" role="link" tabindex="0" jsdata="EdZxp;_;ByyJhQ" jsaction="rcuQ6b:npT2md;hOPlV" data-ved="2ahUKEwiQiYzb3oeNAxXPZ0EAHYurCPIQukt6BAgCEAo">
+                        <div class="imso_mh__tm-nm imso-medium-font imso_mh__tm-nm-ew" data-dtype="d3sen">
+                            <div class="ellipsisize liveresults-sports-immersive__team-name-width" data-df-team-mid="/m/0xbm">
+                                <span aria-hidden="true">
+                                    {match.getHomeTeam().getName()} 
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="imso_mh__scr-sep">
+                        <div>
+                            <div class="imso_mh__ma-sc-cont">
+                                <div class="imso_mh__l-tm-sc imso_mh__scr-it imso-light-font">
+                                    {match.getHomeScore().toString()} 
+                                </div>
+                                <div class="imso_mh__scr-it imso_mh__sep imso-light-font">-</div>
+                                <div class="imso_mh__r-tm-sc imso_mh__scr-it imso-light-font">
+                                    {match.getAwayScore().toString()} 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="imso_mh__second-tn-ed imso_mh__tnal-cont imso-loa imso-ut imso-tnol" jscontroller="QhKwbc" data-df-team-mid="/m/01_1kk" role="link" tabindex="0" jsdata="EdZxp;_;ByyJhU" jsaction="rcuQ6b:npT2md;hOPlV" data-ved="2ahUKEwiQiYzb3oeNAxXPZ0EAHYurCPIQukt6BAgCEAs">
+                        <div class="imso_mh__tm-nm imso-medium-font imso_mh__tm-nm-ew" data-dtype="d3sen">
+                            <div class="ellipsisize liveresults-sports-immersive__team-name-width" data-df-team-mid="/m/01_1kk">
+                                <span aria-hidden="true">
+                                    {match.getAwayTeam().getName()} 
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
     );
 };
 
-export default MatchCard;
+export default PlayerStatsCard;
